fbb008342d4ab054de93487945c88021b27ce93d
Ahmed Asif <ahmedaanasif@gmail.com>
2023-11-12 19:53:46 +0500

    implemented slideshow(tried)

--- src/components/HomePage/Recommendations.js
+++ src/components/HomePage/Recommendations.js
@@ -1,18 +1,9 @@
 import classes from "./Recommendations.module.css";
 import Button from "../utils/Button";
 import { useNavigate } from "react-router-dom";
-import { Spoiler } from "@mantine/core";
 const Recommendations = ({ details }) => {
   const navigate = useNavigate();
-  if (details.title.length > 18) {
-    details.title = details.title.slice(0, 17);
-    details.title += "...";
-  }
-  const description = (
-    <Spoiler maxHeight={window.innerHeight * 0.3} p={0} m={0}>
-      {details.synopsis}
-    </Spoiler>
-  );
+
   return (
     <div className={classes.container}>
       <img
@@ -23,7 +14,7 @@ const Recommendations = ({ details }) => {
       <div className={`${classes.dataHolder} container`}>
         <p className={classes.author}>{details.author}</p>
         <h2>{details.title}</h2>
-        <p className={classes.synopsis}>{description}...</p>
+        <p className={classes.synopsis}>{details.synopsis}</p>
         <p className={classes.price}>${details.price} only</p>
         <Button
           className={classes.button}
